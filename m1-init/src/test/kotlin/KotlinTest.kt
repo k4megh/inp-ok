7abe2c3edd46685b29b5cd64fc2ad52333ee8454
k4megh <k4me@rambler.ru>
2025-06-14 23:09:25 +0300

    Добавлен m1-init

---
+++ m1-init/src/test/kotlin/KotlinTest.kt
@@ -0,0 +1,11 @@
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class KotlinTest {
+    @Test
+    fun firstTest() {
+        assertEquals(3, 2 * 2)
+    }
+
+
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,11 @@
+pluginManagement{
+    val kotlinVersion:String by settings
+    plugins {
+        kotlin("jvm") version kotlinVersion
+    }
+}
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 rootProject.name = "inp-ok"
-
+include("m1-init")
