ed7d667a265cfa6a43475bd550d384e064957fba
k4megh <k4me@rambler.ru>
2025-06-14 23:44:07 +0300

    Вынесена версия foojayResolverConventionVersion

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,14 @@
 pluginManagement{
     val kotlinVersion:String by settings
+    val foojayResolverConventionVersion:String by settings
+
     plugins {
         kotlin("jvm") version kotlinVersion
     }
-}
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+    plugins {
+        id("org.gradle.toolchains.foojay-resolver-convention") version foojayResolverConventionVersion
+    }
+
 }
 rootProject.name = "inp-ok"
 include("m1-init")
